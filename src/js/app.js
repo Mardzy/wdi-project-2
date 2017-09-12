f3f2bf48522a2c0f09e3a45d1e37fecab2e870dc
Mardzy <mardlingp@gmail.com>
2017-09-12 14:38:04 +0100

    hide header

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+$(()=>{
+  const $mainHeader = $('.main-header');
+
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) { //use `this`, not `document`
+      $mainHeader.css({
+        'display': 'none'
+      });
+    } else {
+      $mainHeader.css({
+        'display': 'flex'
+      });
+    }
+  });
+});
